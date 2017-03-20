4ab51916ac15c4659f01154d940e223836e0bd44
ISE-PK <pk@internet.se>
2017-03-20 17:30:13 +0100

    end marker

--- cctest.pl
+++ cctest.pl
@@ -4,15 +4,6 @@ use Win32::Console::ANSI;
 use Term::ANSIScreen qw/:color :cursor :screen/;
 
 
-
-# $CONSOLE->Size(80, 25); # force a console size
-
-
-
-#print color('cyan on_black');
-#system ('cls');
-
-
 my $_Form_SearchPattern="C:/*.*";
 my @_PDF_Form_Files = <${_Form_SearchPattern}>;
 
@@ -43,6 +34,9 @@ format VIEW1 =
 
 $~ = 'VIEW1';
 write ;
+
+print "\n". " ". "=" x 100 ;
+print "\n\n<END>";
 
 
 
